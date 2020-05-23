bc15b809b845d93ce8ef334838e4fea07d3a8f37
Nikita Popkov <popkov-ns@mail.ru>
2020-05-23 13:35:01 +0300

    Автоматизация проекта

--- source/js/app.js
+++ source/js/app.js
@@ -4,4 +4,5 @@ let menuBurger = document.querySelector('.main-nav__list');
 buttonBurger.addEventListener('click', function (evt) {
   evt.preventDefault();
   menuBurger.classList.toggle('main-nav__list--open');
+  buttonBurger.classList.toggle('main-nav__burger--open');
 });
