a2e643097a13f1f9162750fd06d4b7df78fecddd
Nikita Popkov <popkov-ns@mail.ru>
2020-05-24 15:11:40 +0300

    Правки по 1 проверке

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,8 @@
 var buttonBurger = document.querySelector('.main-nav__burger');
 var menuBurger = document.querySelector('.main-nav__list');
 
+menuBurger.classList.remove('no-js');
+
 buttonBurger.addEventListener('click', function (evt) {
   evt.preventDefault();
   menuBurger.classList.toggle('main-nav__list--open');
